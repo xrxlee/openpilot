6120d0777808f2c442d7319ebc7f0ac6f9944fab
kegman <8837066+kegman@users.noreply.github.com>
2018-11-16 12:13:00 -0500

    Physical LKAS button disables OP steer and shows dashed lines on HUD (#19)
    * Change one bar braking profile to 3 bars
    
    * Update planner.py
    
    * Update planner.py
    
    * Update planner.py
    
    * Update planner.py
    
    * Update planner.py
    
    * Update planner.py
    
    * Update planner.py
    
    * Update planner.py
    
    * fake lead car vars
    
    * fake lead vars
    
    * Auto Distance Bar Working - tuning values
    
    * Logging to log.txt
    
    * Override hysteresis for one bar follow
    
    * Only set override when decelerating
    
    * syntax error fix for override flag
    
    * Switch to 1.8s for softer braking
    
    * Disable LKAS
    
    * Apply Steer = 0 when user presses LKAS button
    
    * Add Lkas button
    
    * Add LK mode to HUDD data object
    
    * Add Dashed Lanes to HUD
    
    * Adjust HDD object
    
    * return LKAS var in CarState
    
    * Return LK var to CS
    
    * Syntax error fix
    
    * Update carcontroller.py
    
    * testing hud object hardcoded hud_lanes and lkMode
    
    * Restore accidentally deleted steer command
    
    * Update carcontroller.py
    
    * Update carcontroller.py
    
    * Don't show solid lanes if OP steering disabled
    
    * Dashed lines fix during engagement

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -419,6 +419,7 @@ class CarInterface(object):
     ret.cruiseState.standstill = False
     
     ret.readdistancelines = self.CS.read_distance_lines
+    ret.lkMode = self.CS.lkMode
 
     # TODO: button presses
     buttonEvents = []
