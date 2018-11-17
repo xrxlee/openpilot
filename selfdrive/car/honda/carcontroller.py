752f236b16941df114ec4c79eba6c2f255d3da75
kegman <8837066+kegman@users.noreply.github.com>
2018-11-16 21:20:48 -0500

    Disable audible alert when steering disabled

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -123,6 +123,11 @@ class CarController(object):
     # For lateral control-only, send chimes as a beep since we don't send 0x1fa
     if CS.CP.radarOffCan:
       snd_beep = snd_beep if snd_beep is not 0 else snd_chime
+      
+    # Do not send audible alert when steering is disabled
+    if not CS.lkMode:
+      snd_beep = 0
+      snd_chime = 0
 
     #print chime, alert_id, hud_alert
     fcw_display, steer_required, acc_alert = process_hud_alert(hud_alert)
