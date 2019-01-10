ff34ae6eddb2ad959a6ebf8d8f0cf7e3b6e597fe
kegman <8837066+kegman@users.noreply.github.com>
2019-01-10 18:35:37 -0500

    Gernby's FF steering PR candidate (#97)
    * Fixed feed-forward to consider steer_offset
    
    * fixed a small oops on types
    
    * Testing strategies for zero-crossing
    
    * Moved angle stat collection to latcontrol.py
    
    * First version that runs
    
    * added outbound interface for feed-forward table
    
    * First working
    
    * Added more metrics
    
    * Adjusted parameter timing
    
    * performance tweaks
    
    * Cleanup
    
    * untested tweaks
    
    * minor tweaks
    
    * many untested changes
    
    * going the other way with some things...
    
    * Best yet
    
    * cleaned up personalized "stuff"
    
    * more cleanup for general use
    
    * untested: minor adjustment to further reduce noise
    
    * Fixed defect in desired angle interpolation
    
    * some cleanup
    
    * reverted change to Ki
    
    * Reverted changes to manager.py
    
    * Added steering rate consideration to latcontrol
    
    * cleaned up for PR
    
    * Fixed merge
    
    * Testing approach when desired angle close to actual
    
    * trying rate-based feed-forward
    
    * added self-tuning parms for rate and angle FF
    
    * fixed trim logic, and persisted to flash
    
    * working amazingly well
    
    * decreased time lapse for angle-based FF adjust
    
    * many tweaks, self-tuning is a bitch
    
    * simulteneous dual mode feedforward working very well
    
    * added angular accelleration limit
    
    * added super awesome angular accel limit
    
    * non-specific save
    
    * switching directions again
    
    * oops
    
    * ugly code with amazing results
    
    * awesome, but untested after some cleanup
    
    * more cleanup
    
    * cleanup of the cleanup?  Need to test
    
    * works amazingly well ... big O face
    
    * cleanup
    
    * I wish git was batter for cleanup
    
    * hopefully proper merge of merged fixes of merge
    
    * fixed an oops for non-bosch hondas
    
    * added steer rate to future state calculation
    
    * added actual acceleration to future projected state
    
    * fixed projected angle error for PIF
    
    * untested ...
    
    * added comments
    
    * completely UNtested
    
    * untested merge
    
    * battling shitty git
    
    * git is sucking giant monkey balls
    
    * Trying to figure out what git did...
    
    * still trying to un-git myself
    
    * Finally got it fixed, works awesome!
    
    * Hopefully not corrupted by git
    
    * hopefully fixed white space corruption
    
    * working again, hopefully resolved git issue
    
    * removed hard-coded actuator delay
    
    * Fixed steer_actuator_delay for pathplanner
    
    * Wiggle vision + resonant FF and error cancellation
    
    * increased projection factor and decreased accel_limit
    
    * enhanced torque logic to use accelerated rate
    
    * added smoothing of mpc delta values
    
    * trying different interpolation logic
    
    * added update to cur_state.delta
    
    * very smooth interpolation between delayed MPC updates
    
    * increased Kf for Pilot
    
    * much better interpolation plus Civic tweaks
    
    * tiny meaningless changes
    
    * Fixed honda interface values
    
    * removed steer_offset from pilot
    
    * Tweaked steer_offset list
    
    * Reverted a couple resonant parameters
    
    Reduced projection_factor and increased accel_limit
    
    * Enabled error correction
    
    * changed Kp for some
    
    * Works great... time to clean it up
    
    * reverted accelerated_angle_rate for pid updates
    
    * reverted visiond
    
    * Increased error correction
    
    * Reverted projection_factor
    
    * fixed projected_angle_steers for future statefixed projected_angle_steers for future state
    
    * Added simplified tuning parameters
    
    * last commit didn't have saved values
    
    * not working ... lane filtering
    
    * added calculated steer rate
    
    * updated toyota carstate
    
    * add dual-mode steer-rate alternating
    
    * reverted toyota carstate change
    
    * enabled auto-selection of steer_rate
    
    * removed extra variable
    
    * untested: cleaning up for PR
    
    * Fixed a cleanup issue
    
    * added comments in carstate
    
    * more cleanup
    
    * not quite ready
    
    * added vibration
    
    * need to test, but I think it's ready
    
    * Update carstate.py

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -186,6 +186,10 @@ class CarInterface(object):
     ret.steerKiBP, ret.steerKpBP = [[0.], [0.]]
 
     ret.steerKf = 0.00006 # conservative feed-forward
+    ret.steerReactance = 1.0
+    ret.steerInductance = 1.0
+    ret.steerResistance = 1.0
+    ret.eonToFront = 0.5
 
     if candidate == CAR.CIVIC:
       stop_and_go = True
@@ -226,7 +230,12 @@ class CarInterface(object):
       ret.centerToFront = ret.wheelbase * 0.39
       ret.steerRatio = 15.96  # 11.82 is spec end-to-end
       tire_stiffness_factor = 0.8467
-      ret.steerKpV, ret.steerKiV = [[0.6], [0.18]]
+      ret.steerReactance = 1.0
+      ret.steerInductance = 1.0
+      ret.steerResistance = 0.5
+      ret.eonToFront = 1.0
+      ret.steerKpV, ret.steerKiV = [[0.64], [0.192]]
+      ret.steerKf = 0.000064
       ret.longitudinalKpBP = [0., 5., 35.]
       ret.longitudinalKpV = [1.2, 0.8, 0.5]
       ret.longitudinalKiBP = [0., 35.]
