5148c6bf570e9231d81484199f23803b15ee1a17
kegman <8837066+kegman@users.noreply.github.com>
2019-03-01 09:52:40 -0500

    Try 0.4, 0.15 to reduce ping-pong on Pilot

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -306,7 +306,7 @@ class CarInterface(object):
       ret.centerToFront = ret.wheelbase * 0.41
       ret.steerRatio = 16.0         # as spec
       tire_stiffness_factor = 0.82
-      ret.steerKpV, ret.steerKiV = [[0.5], [0.22]]
+      ret.steerKpV, ret.steerKiV = [[0.4], [0.15]]
       ret.longitudinalKpBP = [0., 5., 35.]
       ret.longitudinalKpV = [1.2, 0.8, 0.5]
       ret.longitudinalKiBP = [0., 35.]
