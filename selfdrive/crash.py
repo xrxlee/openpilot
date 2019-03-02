f61cba84774b33255735bf896bc12a33aa8ea87e
kegman <8837066+kegman@users.noreply.github.com>
2019-03-02 08:28:50 -0500

    Send kegman-plusClarity specific crash logs to Kegman

--- selfdrive/crash.py
+++ selfdrive/crash.py
@@ -18,7 +18,7 @@ if os.getenv("NOLOG") or os.getenv("NOCRASH"):
 else:
   from raven import Client
   from raven.transport.http import HTTPTransport
-  client = Client('https://6731023ca4724c229d6752fda6c4cc17:168c4775059847f1b3d9a9dbbe1f7a85@sentry.io/1405232',
+  client = Client('https://0d5d4e7fd43a46f396705f20aa2d1804:3678c42dcf8d4253bb375e2a1e11ad1f@sentry.io/1406443',
                   install_sys_hook=False, transport=HTTPTransport, release=version, tags={'dirty': dirty})
 
   def capture_exception(*args, **kwargs):
