d4e3d3c4a783bc4d5a1b862cc74bcea358e017dd
kegman <8837066+kegman@users.noreply.github.com>
2019-03-06 07:19:03 -0500

    Update sentry.io crash logs for Gernby branch

--- selfdrive/crash.py
+++ selfdrive/crash.py
@@ -17,7 +17,7 @@ if os.getenv("NOLOG") or os.getenv("NOCRASH"):
 else:
   from raven import Client
   from raven.transport.http import HTTPTransport
-  client = Client('https://6731023ca4724c229d6752fda6c4cc17:168c4775059847f1b3d9a9dbbe1f7a85@sentry.io/1405232',
+  client = Client('https://69bc2e046a1e43df9759bbcfb4b84bc8:095b70312c16499fbc14bf44bde7a4ed@sentry.io/1406472',
                   install_sys_hook=False, transport=HTTPTransport, release=version, tags={'dirty': dirty})
 
   def capture_exception(*args, **kwargs):
