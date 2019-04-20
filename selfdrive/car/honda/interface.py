cf590a74e7898d1e7e81b3d8ce1fab86455ae453
kegman <8837066+kegman@users.noreply.github.com>
2019-04-20 17:32:55 -0400

    Ridgeline rateFF = 0.4

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -358,7 +358,7 @@ class CarInterface(object):
       ret.centerToFront = ret.wheelbase * 0.41
       ret.steerRatio = 15.59        # as spec
       tire_stiffness_factor = 0.82
-      ret.rateFFGain = 0.04
+      ret.rateFFGain = 0.4
       ret.steerMPCReactTime = -0.1     # increase total MPC projected time by 25 ms
       ret.steerMPCDampTime = 0.28       # dampen desired angle over 250ms (5 mpc cycles)
       ret.steerReactTime = 0.001
