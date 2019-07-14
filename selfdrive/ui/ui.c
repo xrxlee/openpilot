8f1804263b51221866ed773ff268009233c5f9af
kegman <8837066+kegman@users.noreply.github.com>
2019-07-13 20:40:40 -0400

    Update ui.c

--- selfdrive/ui/ui.c
+++ selfdrive/ui/ui.c
@@ -2536,7 +2536,7 @@ static void ui_update(UIState *s) {
       return;
     }
     if (ret == 0) {
-      return;
+      continue;
     }
 
     if (polls[0].revents || polls[1].revents || polls[2].revents ||
@@ -2576,6 +2576,7 @@ static void ui_update(UIState *s) {
       {
         s->scene.gpsAccuracy = 99.8;
       }
+      capn_free(&ctx);
       zmq_msg_close(&msg);
     }
 
