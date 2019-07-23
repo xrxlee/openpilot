715528a297b0c66391323f062b055f501efd16c3
Gernby <gernby2002@yahoo.com>
2019-07-22 22:17:13 -0500

    Kegman plus gernby poly center 0.6 (#201)
    * Prevent OEM camera error
    
    * updated keggy-boy branch

--- selfdrive/controls/lib/model_parser.py
+++ selfdrive/controls/lib/model_parser.py
@@ -6,7 +6,6 @@ from selfdrive.kegman_conf import kegman_conf
 kegman = kegman_conf()
 CAMERA_OFFSET = float(kegman.conf['cameraOffset'])  # m from center car to camera
 
-
 class ModelParser(object):
   def __init__(self):
     self.d_poly = [0., 0., 0., 0.]
@@ -16,9 +15,9 @@ class ModelParser(object):
     self.lead_dist, self.lead_prob, self.lead_var = 0, 0, 1
     self._path_pinv = compute_path_pinv()
 
-    self.lane_width_estimate = 2.85
+    self.lane_width_estimate = 3.0
     self.lane_width_certainty = 1.0
-    self.lane_width = 2.85
+    self.lane_width = 3.0
     self.l_prob = 0.
     self.r_prob = 0.
     self.x_points = np.arange(50)
@@ -43,12 +42,14 @@ class ModelParser(object):
 
     # Find current lanewidth
     lr_prob = l_prob * r_prob
-    self.lane_width_certainty += 0.05 * (lr_prob - self.lane_width_certainty)
+    decay_rate = interp(lr_prob, [0., 0.5], [0.1, 0.3])
+    decay_rate *= v_ego / 31.0
+    self.lane_width_certainty += 0.05 * decay_rate * (lr_prob - self.lane_width_certainty)
     current_lane_width = abs(l_poly[3] - r_poly[3])
-    self.lane_width_estimate += 0.005 * (current_lane_width - self.lane_width_estimate)
-    speed_lane_width = interp(v_ego, [0., 31.], [2.85, 3.5])
-    self.lane_width = self.lane_width_certainty * self.lane_width_estimate + \
-                        (1 - self.lane_width_certainty) * speed_lane_width
+    self.lane_width_estimate += 0.005 * decay_rate * (current_lane_width - self.lane_width_estimate)
+    speed_lane_width = interp(v_ego, [0., 31.], [3.0, 3.5])
+    self.lane_width = (self.lane_width_certainty * self.lane_width_estimate + 0.2 * speed_lane_width) / (0.2 + self.lane_width_certainty)
+    #self.lane_width = self.lane_width_certainty * self.lane_width_estimate + (1.0 - self.lane_width_certainty) * speed_lane_width
 
     self.lead_dist = md.lead.dist
     self.lead_prob = md.lead.prob
